b737c0d51a26e66fe3f800a3a6e0c8378000c7cb
20151inf0182 <eduardomeurer999@gmail.com>
2018-10-18 08:42:37 -0300

    Initial commit

---
+++ venv/voz.py
@@ -0,0 +1,6 @@
+import pyttsx3
+en=pyttsx3.init()
+en.setProperty('voice',b'brazil')
+en.say("Bom dia Turma")
+en.say("Bem Vindos ao Instituto Federal de Educação - IFSUL campus Sapiranga")
+en.say("Quarto ano do curso técnico em Informática")
